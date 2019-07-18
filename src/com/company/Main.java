12a97d2e6aa365b4e7468381590d57e6523b382e
IshaanGulati-WAL <ishaan.g@westagilelabs.com>
2019-07-18 23:53:26 +0530

    Added code for merging two sorted arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        MergeTwoSoretdArrays merge = new MergeTwoSoretdArrays();
+        int[] arr1 = new int[] {1,2,4,7};
+        int[] arr2 = new int[] {0,3,5,6};
+        for (int i : merge.mergeTwo(arr1, arr2)) {
+            System.out.println(i);
+        }
     }
 }
